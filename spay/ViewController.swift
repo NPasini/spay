3f20de05401b54f3b4782efdeaf172930614e664
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-18 21:44:52 +0200

    Initial Commit

---
+++ spay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 18/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
