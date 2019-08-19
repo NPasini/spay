e9133f259b2d8b29ef2b63837c30bb2fdf4c9f38
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-19 18:46:45 +0200

    Created SPError class for handling all the error in the app

---
+++ spay/Utilities/Errors/SPError.swift
@@ -0,0 +1,17 @@
+//
+//  SPError.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 19/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import Foundation
+
+private let SPDomain: String = "com.sp"
+
+class SPError: NSError {
+    convenience init(networkError: NetworkError) {
+        self.init(domain: SPDomain, code: networkError.rawValue, userInfo: [NSLocalizedDescriptionKey : String(describing: networkError)])
+    }
+}
