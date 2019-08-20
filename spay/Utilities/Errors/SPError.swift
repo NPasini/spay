8e32a3b71c5b73a2f20bd75e96b69127ddfc3b3e
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-20 23:31:12 +0200

    Utilities: added initialisation of SPError using a StatusError

--- spay/Utilities/Errors/SPError.swift
+++ spay/Utilities/Errors/SPError.swift
@@ -14,4 +14,8 @@ class SPError: NSError {
     convenience init(networkError: NetworkError) {
         self.init(domain: SPDomain, code: networkError.rawValue, userInfo: [NSLocalizedDescriptionKey : String(describing: networkError)])
     }
+    
+    convenience init(statusError: StatusError, statusCode: Int) {
+        self.init(domain: SPDomain, code: statusError.rawValue, userInfo: [NSLocalizedDescriptionKey : String(describing: statusCode)])
+    }
 }
