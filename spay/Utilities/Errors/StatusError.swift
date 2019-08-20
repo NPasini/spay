231abbcb33123e32cb89a50f55d284862ad3b91e
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-20 23:30:42 +0200

    Utilities: added StatusError

---
+++ spay/Utilities/Errors/StatusError.swift
@@ -0,0 +1,14 @@
+//
+//  StatusError.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 20/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import Foundation
+
+enum StatusError: Int, Error {
+    case notFound
+    case badAnswer
+}
