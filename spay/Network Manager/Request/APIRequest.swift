6371ecb98187dba0bbb1c7643b96b3c6b3b1bf20
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-20 06:47:29 +0200

    NetworkManager: created APIRequest

---
+++ spay/Network Manager/Request/APIRequest.swift
@@ -0,0 +1,76 @@
+//
+//  APIRequest.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 18/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import Foundation
+
+class APIRequest<T>: Requestable, Validatable, Processable where T: CustomDecodable {
+    let host: String
+    let path: String
+    let method: HTTPMethod
+    let headerParameters: HTTPHeaders?
+    let bodyParameters: HTTPBodyParameters?
+    let queryParameters: HTTPQueryParameters?
+    
+    var body: Data? {
+        get {
+            return nil
+        }
+    }
+    
+    init(host: String,
+         path: String,
+         method: HTTPMethod,
+         headerParameters: HTTPHeaders? = nil,
+         bodyParameters: HTTPBodyParameters? = nil,
+         queryParameters: HTTPQueryParameters? = nil) {
+        
+        self.host = host
+        self.path = path
+        self.method = method
+        self.bodyParameters = bodyParameters
+        self.queryParameters = queryParameters
+        
+        let baseParameters: HTTPHeaders = [:]
+        
+        if let additionalParameters = headerParameters {
+            self.headerParameters = additionalParameters.reduce(baseParameters) { (result: HTTPHeaders, tuple: (key: String, value: String)) -> HTTPHeaders in
+                
+                var tmp: HTTPHeaders = result
+                
+                tmp[tuple.key] = tuple.value
+                
+                return tmp
+            }
+        } else {
+            self.headerParameters = baseParameters
+        }
+    }
+    
+    func validateResponse(_ response: URLResponse) -> NSError? {
+        return nil
+    }
+    
+    func validateResponseObject(_ object: T) -> NSError? {
+        return nil
+    }
+    
+    func processRequest(_ request: URLRequest) -> URLRequest {
+        return request
+    }
+}
+
+class GetRequest<T>: APIRequest<T> where T: CustomDecodable {
+    init(host: String,
+         path: String,
+         headerParameters: HTTPHeaders? = nil,
+         bodyParameters: HTTPBodyParameters? = nil,
+         queryParameters: HTTPQueryParameters? = nil) {
+        
+        super.init(host: host, path: path, method: HTTPMethod.get, headerParameters: headerParameters, bodyParameters: bodyParameters, queryParameters: queryParameters)
+    }
+}
