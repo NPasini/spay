062622e386ae2bb9fdcefd1d01362bce5a76021b
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-21 22:48:29 +0200

    Beers Page: started to work on Beers List UI

---
+++ spay/Beers Page/ViewControllers/BeersListViewController.swift
@@ -0,0 +1,79 @@
+//
+//  BeersListViewController.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 18/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import UIKit
+
+class BeersListViewController: UIViewController {
+    
+    @IBOutlet weak var offersView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        offersView.layer.cornerRadius = 10
+        
+        setTitle()
+        configureSearchBar()
+        configureTableView()
+    }
+    
+    private func setTitle() {
+        let fullTitle = NSMutableAttributedString(string: "")
+        let title = NSAttributedString(string: "Beer", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .light), NSAttributedString.Key.foregroundColor: UIColor.white])
+        let boldTitle = NSAttributedString(string: " Box", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.white])
+        fullTitle.append(title)
+        fullTitle.append(boldTitle)
+        titleLabel.attributedText = fullTitle
+    }
+    
+    private func configureSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        
+        definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        self.navigationItem.searchController = searchController
+        self.navigationItem.searchController?.searchBar.backgroundColor = UIColor(named: "BackgroundGrey")
+    }
+    
+    private func configureTableView() {
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.backgroundColor = UIColor(named: "BackGroundDarkBlue")
+    }
+}
+
+extension BeersListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension BeersListViewController: UITableViewDelegate {
+    
+}
+
+extension BeersListViewController: UISearchBarDelegate {
+    
+}
+
+extension BeersListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    
+}
+
