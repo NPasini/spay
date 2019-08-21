64b6799778ea5db599f50efb13c3ec70a3e3aed5
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-21 10:04:59 +0200

    Unit Test: added unit test for Beers Response

---
+++ spayTests/Beers Page/API/BeersResponseTest.swift
@@ -0,0 +1,102 @@
+//
+//  BeersResponseTest.swift
+//  spayTests
+//
+//  Created by Pasini, Nicolò on 20/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+@testable import spay
+
+import Quick
+import Nimble
+import Foundation
+
+class BeersResponseTest: QuickSpec {
+    override func spec() {
+        context("Testing the Get Beers API response"){
+            describe("The response should not be decoded"){
+                it("with empty data"){
+                    let response = BeersResponse.decode(Data())
+                    expect(response).to(beNil())
+                }
+            }
+            
+            describe("The response should be decoded") {
+                it("with correct json to parse"){
+                    let path = Bundle(for: type(of: self)).path(forResource: "beersResponse", ofType: "json") ?? ""
+                    let data = (try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe))
+                    
+                    guard let dataObject = data else {
+                        fail("Wrong json file path")
+                        return
+                    }
+                    
+                    let response = BeersResponse.decode(dataObject)
+                    expect(response).notTo(beNil())
+                    expect(response).to(beAnInstanceOf(BeersResponse.self))
+                    
+                    if let beersResponse = response as? BeersResponse {
+                        expect(beersResponse.beers.count).to(equal(25))
+                        
+                        for beer: Beer in beersResponse.beers {
+                            expect(beer.id).notTo(beNil())
+                            expect(beer.name).notTo(beNil())
+                            expect(beer.tagline).notTo(beNil())
+                            expect(beer.description).notTo(beNil())
+                        }
+                    }
+                }
+            }
+            
+            describe("The response should not be decoded") {
+                it("with a json where all the Beer objects have not all the fields"){
+                    let path = Bundle(for: type(of: self)).path(forResource: "beersIncompleteResponse", ofType: "json") ?? ""
+                    let data = (try? Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe))
+
+                    guard let dataObject = data else {
+                        fail("Wrong json file path")
+                        return
+                    }
+
+                    let response = BeersResponse.decode(dataObject)
+                    expect(response).to(beNil())
+                }
+            }
+        }
+        
+        context("The ApiPerfomer with HotelsRequest"){
+            var apiSubscriptionProtocol: APISubscriptionProtocol? = nil
+            
+            AssemblerWrapper.shared.register(assemblies: [NetworkAssembly()])
+            
+            afterEach {
+                apiSubscriptionProtocol?.dispose()
+                apiSubscriptionProtocol = nil
+            }
+            
+            describe("should retrieve value"){
+                it("if response json is an array"){
+                    let request = BeersRequest()
+                    let networkDispatcher: NetworkService? = AssemblerWrapper.shared.resolve(NetworkService.self)
+                    
+                    waitUntil(timeout: 5) { done in
+                        apiSubscriptionProtocol = networkDispatcher?.performApi(request, QoS: .background, completion: { (result) in
+                            
+                            switch result {
+                            case .success(let value):
+                                expect(value).toNot(beNil())
+                                expect(value.beers.count).to(equal(25))
+                            case .failure(_):
+                                fail("Error when performing Beers API")
+                            }
+                            
+                            done()
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
+
