56028c692560d9aafd358515a696d9dfc54f7d99
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-21 23:42:05 +0200

    Beers Page: started to work on BeerTableViewCell UI

---
+++ spay/Beers Page/Views/BeerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeerTableViewCell.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 21/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import UIKit
+
+class BeerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
