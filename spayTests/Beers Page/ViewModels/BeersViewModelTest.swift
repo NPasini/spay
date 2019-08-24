7ba6905ec799ad84ee5031f5f6350536f75cccbc
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-24 16:34:16 +0200

    Unit Test: added tests for BeersViewModel

--- spayTests/Beers Page/ViewModels/BeersViewModelTest.swift
+++ spayTests/Beers Page/ViewModels/BeersViewModelTest.swift
@@ -14,22 +14,36 @@ import Foundation
 
 class BeersViewModelTest: QuickSpec {
     override func spec() {
-        context("Testing the BeersViewModel"){
+        context("Testing the BeersViewModel") {
             let viewModel = BeersViewModel()
             
-            describe("A new instance of the ViewModel is created"){
-                it("The fisrt beer page should be retrieve"){
-                    expect(viewModel.beersModelsList.value.count).toEventually(equal(25), timeout: 1)
+            describe("A new instance of the ViewModel is created") {
+                it("The fisrt beer page should be retrieved") {
+                    expect(viewModel.currentPage).to(equal(1))
+                    expect(viewModel.isFetching).to(equal(true))
+                    expect(viewModel.beersModelsList.value.count).to(equal(0))
+                }
+                
+                it("When the API endpoint returns a response the Beers Models List should be updated with the values") {
+                    expect(viewModel.currentPage).toEventually(equal(2), timeout: 15)
+                    expect(viewModel.isFetching).toEventually(equal(false), timeout: 15)
+                    expect(viewModel.beersModelsList.value.count).toEventually(equal(25), timeout: 15)
                 }
             }
             
             describe("The ViewModel Instance is required to retrieve the next page of Beers from the API") {
-                beforeEach {
-                    viewModel.getBeers(for: 2)
+                it("The ViewModel should start to fetch the new data") {
+                    viewModel.getBeers()
+                    
+                    expect(viewModel.currentPage).to(equal(2))
+                    expect(viewModel.isFetching).to(equal(true))
+                    expect(viewModel.beersModelsList.value.count).to(equal(25))
                 }
                 
-                it("The new Beer Models should be added to the property beersModelsList") {
-                    expect(viewModel.beersModelsList.value.count).toEventually(equal(50), timeout: 30)
+                it("When the API endpoint returns a response, the new Beer Models should be added to the property beersModelsList") {
+                    expect(viewModel.currentPage).toEventually(equal(2), timeout: 15)
+                    expect(viewModel.isFetching).toEventually(equal(false), timeout: 15)
+                    expect(viewModel.beersModelsList.value.count).toEventually(equal(50), timeout: 15)
                 }
             }
         }
