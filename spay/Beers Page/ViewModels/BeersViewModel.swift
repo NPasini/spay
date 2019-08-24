bc9bb6bb7cef07ccad8f770fc150d166efe74f16
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-24 10:26:35 +0200

    Beers Page: fixed issue in fetching new Beers inside ViewModel and added logic to prevent multiple calls to get the same page of Beers

--- spay/Beers Page/ViewModels/BeersViewModel.swift
+++ spay/Beers Page/ViewModels/BeersViewModel.swift
@@ -9,51 +9,61 @@
 import Foundation
 import ReactiveSwift
 
-typealias BeersSignal = Signal<Result<[Beer], NSError>, Never>
-
 class BeersViewModel {
+    var isFetching: Bool
     let beersModelsList: MutableProperty<[Beer]>
     
-    private var disposable: Disposable?
+    private var currentPage: Int
     private let repository: BeerRepository
-    private let serialDisposable: SerialDisposable
-    private let beersSignalPipe: (output: BeersSignal, input: BeersSignal.Observer)
+    private let beers: MutableProperty<[Beer]>
+    private var serialDisposable: SerialDisposable
+    private var addBeersModelsdisposable: Disposable?
 
     init() {
+        currentPage = 1
+        isFetching = false
+        beers = MutableProperty([])
         repository = BeerRepository()
-        beersSignalPipe = BeersSignal.pipe()
         beersModelsList = MutableProperty([])
         serialDisposable = SerialDisposable(nil)
         
-        serialDisposable.inner = repository.getBeers().start(beersSignalPipe.input)
+        addBeersModelsdisposable = beers.signal.observeValues({ [weak self] (newBeers: [Beer]) in
+            self?.currentPage += 1
+            self?.isFetching = false
+            self?.beersModelsList.value.append(contentsOf: newBeers)
+        })
         
-        disposable = beersSignalPipe.output.signal.observeValues { [weak self] (result: Result<[Beer], NSError>) in
-            switch result {
-            case .success(let newBeers):
-                self?.beersModelsList.value.append(contentsOf: newBeers)
-            case .failure:
-               break
-            }
-        }
+        getBeers()
+    }
+    
+    deinit {
+        dispose()
     }
     
     //MARK: Public Functions
-    func getBeers(for page: Int) {
-        serialDisposable.inner = repository.getBeers(page: page).start(beersSignalPipe.input)
+    func getBeers() {
+        if (!isFetching) {
+            isFetching = true
+            
+            serialDisposable.inner = beers <~ repository.getBeers(page: currentPage).map({ (result: Result<[Beer], NSError>) -> [Beer] in
+                switch result {
+                case .success(let newBeers):
+                    return newBeers
+                case .failure:
+                    return []
+                }
+            })
+        }
     }
 
     //MARK: Private Functions
     private func dispose() {
+        if let disposable = addBeersModelsdisposable, !disposable.isDisposed {
+            disposable.dispose()
+        }
+        
         if (!serialDisposable.isDisposed) {
             serialDisposable.dispose()
         }
-        
-        if let d = disposable, !d.isDisposed {
-            d.dispose()
-        }
-    }
-
-    deinit {
-        dispose()
     }
 }
