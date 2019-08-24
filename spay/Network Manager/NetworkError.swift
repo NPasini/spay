c83b043f5bbded41b9dba9a125f6be6fa9822a53
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-24 17:21:07 +0200

    Network Manager: removed unnecessary Errors

--- spay/Network Manager/NetworkError.swift
+++ spay/Network Manager/NetworkError.swift
@@ -9,31 +9,23 @@
 import Foundation
 
 enum NetworkError: Int, Error {
-    case statusCode
     case missingData
-    case endOfStream
     case parserError
     case unknownError
     case emptyResponse
     case invalidRequest
-    case missingRequest
     case invalidResponse
     case invalidResponseObject
-    case invalidInputParameters
     
     var description: String {
         switch self {
-        case .endOfStream: return "End of stream."
         case .unknownError: return "Unknown error."
         case .parserError: return "Parser error data."
         case .emptyResponse: return "No objects found."
         case .missingData: return "Empty data retrieved."
         case .invalidResponse: return "Invalid response."
-        case .statusCode: return "Wrong http status code."
-        case .missingRequest: return "No request available"
         case .invalidRequest: return "Failed to generate request."
         case .invalidResponseObject: return "Data parsed are invalid."
-        case .invalidInputParameters: return "Invalid input parameters."
         }
     }
 }
