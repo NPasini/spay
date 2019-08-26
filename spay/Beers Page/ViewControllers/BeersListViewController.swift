7f96666cfad5f91ce586f37c7729ca06a09a876d
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-27 01:15:09 +0200

    Beers Page: passing string formatted in proper way from SearchBar to ViewModel

--- spay/Beers Page/ViewControllers/BeersListViewController.swift
+++ spay/Beers Page/ViewControllers/BeersListViewController.swift
@@ -121,7 +121,7 @@ extension BeersListViewController: UITableViewDataSourcePrefetching {
 
 extension BeersListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        viewModel?.searchTextPipe.input.send(value: searchText.trimmingCharacters(in: .whitespacesAndNewlines))
+        viewModel?.searchTextPipe.input.send(value: searchText.trimmingCharacters(in: .newlines))
     }
 }
 
