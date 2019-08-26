f89da7da88032e0456c8d2f4ead2c20375eb51f9
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-26 19:46:25 +0200

    Beers Page: added Malts info in Beer Model

--- spay/Beers Page/Models/Beer.swift
+++ spay/Beers Page/Models/Beer.swift
@@ -9,9 +9,49 @@
 import Foundation
 
 struct Beer: Decodable {
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case tagline
+        case image_url
+        case description
+        case ingredients
+    }
+    
+    enum AdditionalCodingKeys: CodingKey {
+        case malt
+    }
+    
     let id: Int
     let name: String
+    let malts: [Malt]
     let tagline: String
-    let image_url: String
+    let imageUrl: String
     let description: String
+    
+    init(id: Int,
+         name: String,
+         malts: [Malt],
+         tagline: String,
+         imageUrl: String,
+         description: String) {
+        self.id = id
+        self.name = name
+        self.malts = malts
+        self.tagline = tagline
+        self.imageUrl = imageUrl
+        self.description = description
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        tagline = try container.decode(String.self, forKey: .tagline)
+        imageUrl = try container.decode(String.self, forKey: .image_url)
+        description = try container.decode(String.self, forKey: .description)
+        
+        let additionalContainer = try container.nestedContainer(keyedBy: AdditionalCodingKeys.self, forKey: .ingredients)
+        malts = try additionalContainer.decode([Malt].self, forKey: .malt)
+    }
 }
