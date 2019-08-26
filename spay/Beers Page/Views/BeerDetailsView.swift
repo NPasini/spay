884be2e9d212165de91c3b2e74be17de20378a42
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-26 18:23:56 +0200

    Beers Page: improved Beer Detail View and added animation when opening and closing

--- spay/Beers Page/Views/BeerDetailsView.swift
+++ spay/Beers Page/Views/BeerDetailsView.swift
@@ -17,21 +17,26 @@ class BeerDetailsView: UIView {
     @IBOutlet weak var bookmarkView: UIView!
     @IBOutlet weak var beerImage: UIImageView!
     @IBOutlet weak var beerDescription: UILabel!
+    @IBOutlet weak var detailsViewHeightConstraint: NSLayoutConstraint!
+    
+    weak var delegate: BeerDetailsViewDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         detailsView.layer.cornerRadius = 10
+        detailsViewHeightConstraint.constant = 0
+        detailsView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
         let _ = UIView.Inflate(type: BookmarkView.self, owner: self, inside: bookmarkView)
     }
     
-    func configure(with model: Beer) {
+    func showDetails(for model: Beer) {
         name.text = model.name
         tagline.text = model.tagline
         beerDescription.text = model.description
         
-        if let imageURL = URL(string: model.image_url) {
+        if let imageURL = URL(string: model.imageUrl) {
             beerImage.sd_setImage(with: imageURL) { [weak self] (image: UIImage?, error: Error?, cacheType: SDImageCacheType, imageURL: URL?) in
                 guard error == nil else { return }
                 
@@ -39,6 +44,25 @@ class BeerDetailsView: UIView {
             }
         } else {
             beerImage.image = UIImage()
+        }
+        
+        show()
+    }
+    
+    private func show() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.detailsViewHeightConstraint.constant = 285
+            self.layoutIfNeeded()
+        })
+    }
+    
+    @IBAction func close() {
+        UIView.animate(withDuration: 0.3, delay: 0, animations: {
+            self.detailsViewHeightConstraint.constant = 0
+        }) { (completed: Bool) in
+            if (completed) {
+                self.delegate?.closeDetailsView()
+            }
         }
     }
 }
