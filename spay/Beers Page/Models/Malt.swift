a9afa09a5d704df58bc72e343871b643ed5bc1dd
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-26 19:46:00 +0200

    Beers Page: added parsing of Malts data from API response in order to be able to filter on malts number in composition

---
+++ spay/Beers Page/Models/Malt.swift
@@ -0,0 +1,13 @@
+//
+//  Malt.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 26/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import Foundation
+
+struct Malt: Decodable {
+    let name: String
+}
