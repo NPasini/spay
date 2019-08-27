a957a8ed4f57ee100654c2c86e6b9d0e9cc8c6ec
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-27 19:05:48 +0200

    Beers Page: configured FilterCollectionViewCell

--- spay/Beers Page/Views/FilterCollectionViewCell.swift
+++ spay/Beers Page/Views/FilterCollectionViewCell.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class FilterCollectionViewCell: UICollectionViewCell {
-
+class FilterCollectionViewCell: UICollectionViewCell, Identifiable {
+    
+    @IBOutlet weak var content: UIView!
+    @IBOutlet weak var filterLabel: UILabel!
+    
+    var filter: Filter?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
+    override func prepareForReuse() {
+        content.backgroundColor = UIColor(named: "BackgroundGrey")
+    }
+    
+    //MARK: Public Functions
+    func configure(with filter: Filter) {
+        self.filter = filter
+        content.layer.cornerRadius = 20
+        filterLabel.text = filter.filterValue.filterText
+        
+        setUI(for: filter.selected)
+    }
+    
+    func tapOnFilter() {
+        if let filterModel = filter {
+            filterModel.changeSelection()
+            setUI(for: filterModel.selected)
+        }
+    }
+    
+    //MARK: Private Functions
+    private func setUI(for selected: Bool) {
+        if (selected) {
+            filterLabel.textColor = UIColor.black
+            content.backgroundColor = UIColor(named: "BackGroundOrange")
+        } else {
+            filterLabel.textColor = UIColor.white
+            content.backgroundColor = UIColor(named: "BackgroundGrey")
+        }
+    }
 }
