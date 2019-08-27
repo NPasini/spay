92c1303638175121386862edffccc8dd2e7261cc
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-27 13:52:30 +0200

    Beers Page: added View for filters

---
+++ spay/Beers Page/Views/FilterCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilterCollectionViewCell.swift
+//  spay
+//
+//  Created by Pasini, Nicolò on 27/08/2019.
+//  Copyright © 2019 Pasini, Nicolò. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
