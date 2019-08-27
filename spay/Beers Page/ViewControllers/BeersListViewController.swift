3b0fe88230d40f7e0f41b25e65169f6f338b2c1a
Nicolò Pasini <nicolo.pasini@accenture.com>
2019-08-27 19:05:26 +0200

    Beers Page: configured Filters Collection View and added stubbed filters

--- spay/Beers Page/ViewControllers/BeersListViewController.swift
+++ spay/Beers Page/ViewControllers/BeersListViewController.swift
@@ -19,6 +19,7 @@ class BeersListViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var filtersCollectionView: UICollectionView!
     
+    var filters: [Filter] = []
     var disposable: Disposable?
     var viewModel: BeersViewModel?
     var beerDetailsView: BeerDetailsView?
@@ -29,9 +30,11 @@ class BeersListViewController: UIViewController {
         
         viewModel = BeersViewModel()
         
+        setFilters()
         configureUI()
         configureSearchBar()
         configureTableView()
+        configureCollectionView()
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,6 +44,15 @@ class BeersListViewController: UIViewController {
     }
     
     //MARK: Private Functions
+    private func setFilters() {
+        let f1 = Filter(value: .oneMalt)
+        let f2 = Filter(value: .twoMalts)
+        let f3 = Filter(value: .threeMalts)
+        let f4 = Filter(value: .fourOrMoreMalts)
+        
+        filters = [f1, f2, f3 ,f4]
+    }
+    
     private func configureUI() {
         setTitle()
         offersView.layer.cornerRadius = 10
@@ -76,14 +88,27 @@ class BeersListViewController: UIViewController {
         tableView.register(viewType: BeerTableViewCell.self)
         
         if let vm = viewModel {
-            disposable = tableView.reactive.reloadData <~ vm.beersModelsList.signal.map({_ in
+            disposable = tableView.reactive.reloadData <~ vm.beersDataSource.signal.map({_ in
                 OSLogger.uiLog(message: "Reloading TableView", access: .public, type: .debug)
                 return })
         }
     }
     
+    private func configureCollectionView() {
+        filtersCollectionView.delegate = self
+        filtersCollectionView.dataSource = self
+        
+        filtersCollectionView.indicatorStyle = .white
+        
+        if let flowLayout = filtersCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = CGSize(width: 150, height: 45)
+        }
+        
+        filtersCollectionView.register(viewType: FilterCollectionViewCell.self)
+    }
+    
     private func isLoadingCell(at indexPath: IndexPath) -> Bool {
-        if let currentCount = viewModel?.beersModelsList.value.count {
+        if let currentCount = viewModel?.beersDataSource.value.count {
             return indexPath.row >= (currentCount - 1)
         } else {
             return false
@@ -93,11 +118,11 @@ class BeersListViewController: UIViewController {
 
 extension BeersListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel?.beersModelsList.value.count ?? 0
+        return viewModel?.beersDataSource.value.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cellModel = viewModel?.beersModelsList.value[indexPath.row], let cell = tableView.dequeueReusableCell(withIdentifier: BeerTableViewCell.identifier, for: indexPath) as? BeerTableViewCell {
+        if let cellModel = viewModel?.beersDataSource.value[indexPath.row], let cell = tableView.dequeueReusableCell(withIdentifier: BeerTableViewCell.identifier, for: indexPath) as? BeerTableViewCell {
             cell.delegate = self
             cell.configure(with: cellModel)
             return cell
@@ -115,6 +140,44 @@ extension BeersListViewController: UITableViewDataSourcePrefetching {
         if (indexPaths.contains(where: isLoadingCell(at:))) {
             OSLogger.uiLog(message: "Fetching new Data Models", access: .public, type: .debug)
             viewModel?.getBeers()
+        }
+    }
+}
+
+extension BeersListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterCollectionViewCell.identifier, for: indexPath) as? FilterCollectionViewCell {
+            cell.configure(with: filters[indexPath.row])
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+}
+
+extension BeersListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = 150
+        let height: CGFloat = 45
+        
+        if let collectionView = Bundle.main.getView(from: FilterCollectionViewCell.self, owner: nil) {
+            collectionView.configure(with: filters[indexPath.row])
+            width = collectionView.filterLabel.intrinsicContentSize.width + 30
+        }
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+extension BeersListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? FilterCollectionViewCell {
+            cell.tapOnFilter()
+            viewModel?.getBeersWith(filter: filters[indexPath.row])
         }
     }
 }
